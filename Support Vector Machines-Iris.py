6ca852089b3117c729e262dcd1cf939cf1ccc2a8
onehorselee <onehorselee@gmail.com>
2017-05-04 17:09:47 -0400

    Update and rename Support Vector Machine-Iris.py to Support Vector Machines-Iris.py

--- Support Vector Machines-Iris.py
+++ Support Vector Machines-Iris.py
@@ -1,10 +1,13 @@
-### The Basic Model
+###################################################################
+##################### The Basic Model##############################
 from sklearn import datasets
 from sklearn import svm
 
-#load Iris dataset
+# load Iris dataset
 iris = datasets.load_iris()
 x,y = iris.data, iris.target
+
+# train the model
 clf.fit(x,y)
 predict = clf.predict(iris.data)
 
