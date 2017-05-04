f0622a722c664b319bc271f287b8a38c70b6b280
onehorselee <onehorselee@gmail.com>
2017-05-04 17:08:38 -0400

    Create Support Vector Machine-Iris.py

---
+++ Support Vector Machine-Iris.py
@@ -0,0 +1,12 @@
+### The Basic Model
+from sklearn import datasets
+from sklearn import svm
+
+#load Iris dataset
+iris = datasets.load_iris()
+x,y = iris.data, iris.target
+clf.fit(x,y)
+predict = clf.predict(iris.data)
+
+# caculate accuracy 
+print("Accuracy:", sum(predict == iris.target)/len(iris.data))
